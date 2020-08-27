f6776afb89aa14935dc654aa765980b3e42b8107
marek <marek.michalowski@gmail.com>
2020-08-27 13:01:35 +0200

    Adding Class MyController
    	first look on Dependency Injection with MyController Class and
    	geting instance from ApplicationContext.

--- src/main/java/com/example/demo/DependencyInjectionApplication.java
+++ src/main/java/com/example/demo/DependencyInjectionApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.demo.controllers.MyController;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DependencyInjectionApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DependencyInjectionApplication.class, args);
+		MyController myController = (MyController) ctx.getBean("myController");
+		String greeting = myController.sayHello();
+		System.out.println( greeting );
 	}
 
 }
